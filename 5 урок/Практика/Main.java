2a6babafcba6b4ed7181e97b19766da3936cc5f3
minimum-avail9 <144429813+minimum-avail9@users.noreply.github.com>
2023-10-06 11:39:52 +0300

    Add files via upload

---
+++ 5 урок/Практика/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for(int n = 0, b = scanner.nextInt(); n < b; n++) {
+            System.out.println(n);
+        }
+    }
+}
---
+++ 5 урок/Практика/Main2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int b = scanner.nextInt();
+        for(int s = 10; s > b; s--) {
+            System.out.println(s);
+
+        }
+
+    }
+
+}
